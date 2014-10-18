ef171e6dc3e9dcf4c79a2d8952a86d7618ce92d0
schqiushui <orochi9999@gmail.com>
2014-10-18 17:22:38 +0800

    first commit:add kernel_a5dwg-3.4.0-gfcd98b7 sources from htcdev.com

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Sep 30 15:53:35 CST 2014"
+#define LINUX_COMPILE_BY "jiangxu"
+#define LINUX_COMPILE_HOST "jiangxu"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-gfcd98b7-dirty"
